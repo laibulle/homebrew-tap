3a0627635bbd560bc40dfeac8ca729086c57a94f
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2026-03-17 16:52:49 +0100

    first commit

--- Formula/protokol-cli.rb
+++ Formula/protokol-cli.rb
@@ -1,25 +1,25 @@
-require "language/node"
-
 class ProtokolCli < Formula
-  desc "Command line interface for the Protocol project"
-  homepage "https://github.com/laibulle/protocol"
-  # Replace 'v1.0.0' with your actual latest tag
-  url "https://github.com/laibulle/protocol/archive/refs/tags/v1.0.0.tar.gz"
+  desc "Command line interface for the Protokol project"
+  homepage "https://github.com/laibulle/protokol"
+  url "https://github.com/laibulle/protokol/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "REPLACE_WITH_ACTUAL_SHA256"
   license "MIT"
 
   depends_on "node"
 
   def install
-    # Installs the app and its dependencies into a private location (libexec)
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    
-    # Creates a symlink in /usr/local/bin (or equivalent) to your executable
+    system "npm", "install"
+    system "npx", "turbo", "build", "--filter=@protokol/cli"
+
+    cd "apps/cli" do
+      system "npm", "pack"
+      system "npm", "install", "-g", "--prefix", libexec, "protokol-cli-#{version}.tgz"
+    end
+
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    # Basic check to see if the help command works
-    system "#{bin}/protokol-cli", "--help"
+    system bin/"protokol", "--help"
   end
 end
