2871d22f0ed75a21c0a17fdbba177d0e5b21465c
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2026-03-18 10:05:55 +0100

    first commit

--- Formula/protokol-cli.rb
+++ Formula/protokol-cli.rb
@@ -2,7 +2,7 @@ class ProtokolCli < Formula
   desc "Command line interface for the Protokol project"
   homepage "https://github.com/laibulle/protokol"
   url "https://github.com/laibulle/protokol/archive/refs/heads/main.tar.gz"
-  sha256 "055638306ded3dcbcb6e67c832095eafa7a20d56b79a63438adff96bd662450a"
+  sha256 "3e2eb48d7ac3a891a087e0991584158e7d8873f208c2eec102f0b6f2aa4172c2"
   license "MIT"
   version "0.1.0"
 
